21e86351ceaebce01931aec4174cf0b173125441
Joster <korolev.denis.b19.503@gmail.com>
2020-07-02 21:00:48 +0300

    Testing the Brackeys scripts

---
+++ bases/Lesson 2/2DGame with brackeys/Assets/PlayerAnimation/charactrer_move2D.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class charactrer_move2D : MonoBehaviour
+{
+    public Brackeys_script_2D contoller;
+    public float runSpeed = 40f;
+    public float jumpVel = 40f;
+    private float hMove = 0f;
+    
+    public void Update()
+    {
+        hMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        contoller.Move(hMove * Time.fixedDeltaTime, false, false);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            contoller.Move(jumpVel * Time.fixedDeltaTime, true, true);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// abstract.,
+
+
+
+
+
+
+
--- bases/Lesson 2/New Unity Project/Assets/Scripts/Player/Player_move.cs
+++ bases/Lesson 2/New Unity Project/Assets/Scripts/Player/Player_move.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player_move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Serializable]
+    private Vector2 speed = new Vector2(5, 2);
 
-    // Update is called once per frame
     void Update()
     {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        var colider = GetComponent<BoxCollider2D>();
+        Vector3 cur = new Vector3();
+        cur.x = Input.GetAxis("Horizontal") * speed.x;
+        // Debug.Log(Input.GetAxis("Horizontal"));
+        if (Input.GetKey(KeyCode.Space))
+            cur.y = speed.y;
         
+            colider.transform.position += cur;
     }
 }
